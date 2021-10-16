953b2144045e6646490a1e2dce300efa514007eb
Ben Le <hale@cpp.edu>
2021-10-15 23:11:52 -0700

    added parts of load_file and save_file

--- address_book_fop.c
+++ address_book_fop.c
@@ -11,9 +11,10 @@
 Status load_file(AddressBook *address_book)
 {
 	int ret;
+	FILE* fp = fopen(DEFAULT_FILE, "r");
 	if (ret == 0)
 	{
-		FILE* fp = fopen(DEFAULT_FILE, "r");
+		
 		if (fp == NULL)
 		{
 			printf("Failed to open file!%s\n", DEFAULT_FILE);
@@ -22,29 +23,50 @@ Status load_file(AddressBook *address_book)
 	}
 	else
 	{
-		/* Create a file for adding entries */
-	}
+		while (!feof(fp))
+    	{	
+        	if (addr_book->size >= addr_book->capacity)
+        	{
+            	ContactInfo* new_list = (ContactInfo*)malloc(sizeof(PersonInfo)*address_book->capacity);
+				size_t i = 0;
+				for (; i < address_book->count; ++i)
+				{
+					new_list[i] = address_book->list[i];             
+				}
+				free(address_book->list);        
+				address_book->list = new_list;
+			}
+			size_t cur = address_book->count;
+			fscanf(fp, "%s%s%s\n",
+				address_book->list[cur].name,
+				address_book->list[cur].phone_numbers,
+				address_book->list[cur].email_addresses);
+			++address_book->count;
+		}
+	
 
 	return e_success;
 }
 
 Status save_file(AddressBook *address_book)
 {
-	/*
-	 * Write contacts back to file.
-	 * Re write the complete file currently
-	 */ 
+	
 	address_book->fp = fopen(DEFAULT_FILE, "w");
 
 	if (address_book->fp == NULL)
 	{
 		return e_fail;
 	}
+	size_t i = 0;
+    for (; i < address_book->count; ++i)
+    {
+		fprintf(fp, "%s%s%s\n",
+				address_book->list[i].name,
+				address_book->list[i].phone_numbers,
+				address_book->list[i].email_addresses);
+	}
 
-	/* 
-	 * Add the logic to save the file
-	 * Make sure to do error handling
-	 */ 
+	
 
 	fclose(address_book->fp);
 
